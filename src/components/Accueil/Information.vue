d735aae39de35c4cce068c9d24f9d8ebee3d96be
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-05-28 20:11:03 +0200

    feat: ajout des articles responsive

--- src/components/Accueil/Information.vue
+++ src/components/Accueil/Information.vue
@@ -1,41 +1,32 @@
 <template>
-  <div class="information">
-    <h2>Comment ça marche ?</h2>
-    <article class="row justify-center articles items-start">
-      <article class="col-3 column questions">
-        <img
-          class="col"
-          src="./../../assets/icons/question.svg"
-          alt="Icon question"
-        />
-        <p class="col text-center">
+  <div class="q-py-md q-px-xl information">
+    <h2 class="text-center">Comment ça marche ?</h2>
+    <div class="row justify-evenly articles">
+      <article class="text-center questions">
+        <img src="./../../assets/icons/question.svg" alt="Icon question" />
+        <p>
           Je répond à 10 questions, dans l’objectif de trouver mes préférences :
           lieu, saison, activités, centres d’intérêt, rythme de vie..
         </p>
       </article>
-      <article class="col-3 column reponses">
-        <img
-          class="col"
-          src="./../../assets/icons/reponse.svg"
-          alt="Icon reponse"
-        />
-        <p class="col text-center">
+      <article class="text-center reponses">
+        <img src="./../../assets/icons/reponse.svg" alt="Icon reponse" />
+        <p>
           Mes réponses sont comparées avec tous les lieux répertoriés en France.
         </p>
       </article>
-      <article class="col-3 column destination">
+      <article class="text-center destination">
         <img
-          class="col icon-destination"
           src="./../../assets/icons/destination.svg"
           alt="Icon destination"
         />
-        <p class="col text-center">
+        <p>
           LA destination qui me correspond à 100% m’est proposée, avec des
           propositions de logements, de moyens de transports et des activités
           d’entreprises partenaires.
         </p>
       </article>
-    </article>
+    </div>
   </div>
 </template>
 <script lang="ts">
@@ -50,17 +41,21 @@ h2 {
   font-family: $font-family-h2;
   font-size: $font-size-h2;
   color: white;
-  text-align: center;
-  padding: 1em;
 }
 
-p{
+p {
   font-size: $font-size-p;
   font-family: $font-family-p;
 }
 
-.articles{
-  padding-bottom: 20em;
+article {
+  width: 222px;
+  padding: 10px;
+  margin: 15px;
+  background: rgba(255, 255, 255, 0.5);
+  backdrop-filter: blur(7.5px);
+  /* Note: backdrop-filter has minimal browser support */
+  border-radius: 20px;
 }
 
 .information {
@@ -69,19 +64,8 @@ p{
   background-blend-mode: screen;
 }
 
-img{
-  width: 50px;
-  height: 50px;
-}
-
-.column {
-  display: flex;
-  align-items: center;
-  background: rgba(255, 255, 255, 0.5);
-  backdrop-filter: blur(7.5px);
-  /* Note: backdrop-filter has minimal browser support */
-  border-radius: 20px;
-  padding: 0 10px;
-  margin: 10px;
+img {
+  width: 55px;
+  height: 55px;
 }
 </style>
--- src/components/Accueil/PointsCle.vue
+++ src/components/Accueil/PointsCle.vue
@@ -1,47 +1,69 @@
 <template>
-    <div class="row justify-center pointsCle">
-        <img class="q-ma-xl" width="250px" src="./../../assets/img/logo1.svg" alt="fond de carte pour voyage">
-        <div class="row articles">
-            <article class=" text-center">
-                <img height="80px" src="./../../assets/icons/idee.svg">
-                <p >Sol’Air, c’est une solution rapide, gratuite et inédite pour trouver la destination de vos prochaines vacances en France</p>
-            </article>
-            <article class=" text-center">
-                <img height="80px" src="./../../assets/icons/destination-blanc.svg" alt="">
-                <p >Pour chacun de vos voyages, vous pourrez découvrir une destination différente, selon la saison à laquelle vous partez.</p>
-            </article>
-            <article  class=" text-center">
-                <img height="80px" src="./../../assets/icons/top-chrono.svg" alt="">
-                <p >Pour chacun de vos voyages, vous pourrez découvrir une destination différente, selon la saison à laquelle vous partez.</p>
-            </article>
-            <article  class=" text-center">
-                <img height="80px" src="./../../assets/icons/checked.svg" alt="">
-                <p >Pour chacun de vos voyages, vous pourrez découvrir une destination différente, selon la saison à laquelle vous partez.</p>
-            </article>
-        </div>
+  <div class="row justify-center q-py-md q-px-xl pointsCle">
+    <img
+      class="q-ma-xl"
+      width="250px"
+      src="./../../assets/img/logo1.svg"
+      alt="fond de carte pour voyage"
+    />
+    <div class="row justify-evenly articles">
+      <article class="text-center">
+        <img height="80px" src="./../../assets/icons/idee.svg" />
+        <p>
+          Sol’Air, c’est une solution rapide, gratuite et inédite pour trouver
+          la destination de vos prochaines vacances en France
+        </p>
+      </article>
+      <article class="text-center">
+        <img
+          height="80px"
+          src="./../../assets/icons/destination-blanc.svg"
+          alt=""
+        />
+        <p>
+          Pour chacun de vos voyages, vous pourrez découvrir une destination
+          différente, selon la saison à laquelle vous partez.
+        </p>
+      </article>
+      <article class="text-center">
+        <img height="80px" src="./../../assets/icons/top-chrono.svg" alt="" />
+        <p>
+          Pour chacun de vos voyages, vous pourrez découvrir une destination
+          différente, selon la saison à laquelle vous partez.
+        </p>
+      </article>
+      <article class="text-center">
+        <img height="80px" src="./../../assets/icons/checked.svg" alt="" />
+        <p>
+          Pour chacun de vos voyages, vous pourrez découvrir une destination
+          différente, selon la saison à laquelle vous partez.
+        </p>
+      </article>
     </div>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
 export default defineComponent({
-    name: 'PointsCle',
-    components: {},
+  name: 'PointsCle',
+  components: {},
 });
 </script>
 <style lang="scss" scoped>
-.pointsCle{
-    padding: 2em;
-    background: url('./../../assets/img/carte.jpg') center / cover no-repeat;
+.pointsCle {
+  padding: 2em;
+  background: url('./../../assets/img/carte.jpg') center / cover no-repeat;
 }
 
 article {
-    width: 200px;
-    padding: 20px;
+  width: 222px;
+  padding: 10px;
+  margin: 15px;
 }
 
 p {
-    font-size: $font-size-p;
-    font-family: $font-family-p;
-    color: white;
+  font-size: $font-size-p;
+  font-family: $font-family-p;
+  color: white;
 }
 </style>
