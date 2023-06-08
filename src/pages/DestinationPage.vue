97125f02236806ee7bfd972408361d7f08dd44d0
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-06-08 12:31:18 +0200

    feat: ajout du style

--- src/pages/DestinationPage.vue
+++ src/pages/DestinationPage.vue
@@ -1,53 +1,66 @@
 <template>
-    <HeaderCustom :label="destination.ville??''" />
-    <div>
-        <div>
-            <h2>{{ destination.nom }}</h2>
-            <p>{{ destination.description }}</p>
-        </div>
-        <img :src="destination.urlimage">
+  <HeaderCustom :label="destination.ville ?? ''" />
+  <div class="row justify-center items-start q-py-md q-px-xl q-gutter-lg">
+    <div class="column justify-evenly description">
+      <h2 class="text-custom-h2 text-center">{{ destination.nom }}</h2>
+      <p class="text-custom-p text-center">{{ destination.description }}</p>
     </div>
-    <FooterComponent />
-  </template>
-  <script lang="ts">
-  import { defineComponent, onMounted, ref } from 'vue';
-  import HeaderCustom from 'src/components/HeaderCustom.vue';
-  import FooterComponent from 'src/components/FooterComponent.vue';
-  import { useQuasar } from 'quasar';
+    <img class="q-ma-xl" :src="destination.urlimage" />
+  </div>
+  <FooterComponent />
+</template>
+<script lang="ts">
+import { defineComponent, onMounted, ref } from 'vue';
+import HeaderCustom from 'src/components/HeaderCustom.vue';
+import FooterComponent from 'src/components/FooterComponent.vue';
+import { useQuasar } from 'quasar';
 import { Destination } from 'src/api/back_solair/models/Destination';
 import { solairAPI } from 'src/api/back_solair';
 // import { useRoute } from 'vue-router';
-  
-  export default defineComponent({
-    name: 'DestinationPage',
-    setup() {
-      const $q = useQuasar();
-      const destination = ref({} as Destination)
+
+export default defineComponent({
+  name: 'DestinationPage',
+  setup() {
+    const $q = useQuasar();
+    const destination = ref({} as Destination);
     //   const route = useRoute();
     //   const getClicked = ref()
-      onMounted(async () => {
+    onMounted(async () => {
       try {
         $q.loading.show();
-        destination.value = (await solairAPI.destination.getDestinationsRandomAsync())[0]
+        destination.value = (
+          await solairAPI.destination.getDestinationsRandomAsync()
+        )[0];
         // getClicked.value = route.params.body;
         // console.log(getClicked.value);
         // console.log(typeof(getClicked.value));
-
       } catch (e) {
         console.log(e);
       } finally {
         $q.loading.hide();
       }
     });
-      return {
-        destination
-      };
-    },
-    components: {
-      HeaderCustom,
-      FooterComponent,
-    },
-  });
-  </script>
-  <style lang="scss" scoped></style>
-  
+    return {
+      destination,
+    };
+  },
+  components: {
+    HeaderCustom,
+    FooterComponent,
+  },
+});
+</script>
+<style lang="scss" scoped>
+img {
+  width: 35em;
+}
+
+.description {
+  width: 30em;
+}
+
+.row {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
