0d5fa103479c66aa68399a797a1536cb7fb3ed8f
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-06-08 07:00:18 +0200

    feat: ajout de getDestinationRandomAsync

--- src/api/back_solair/ressources/DestinationRessouce.ts
+++ src/api/back_solair/ressources/DestinationRessouce.ts
@@ -1,13 +1,18 @@
 import { httpClient } from '../httpClient';
 import { Destination } from '../models/Destination';
 
-const ressource = 'api/destinations'
+const ressource = 'api/destinations';
 
 class DestinationRessource {
-    public async getDestinationsAsync(): Promise<Destination[]> {
-        const response = await httpClient.get(`${ressource}`)
-        return response.data as Destination[]
-    }
+  public async getDestinationsAsync(): Promise<Destination[]> {
+    const response = await httpClient.get(`${ressource}`);
+    return response.data as Destination[];
+  }
+
+  public async getDestinationsRandomAsync(): Promise<Destination[]> {
+    const response = await httpClient.get(`${ressource}`);
+    return response.data as Destination[];
+  }
 }
 
-export {DestinationRessource}
+export { DestinationRessource };
