1d7d68fbabc6ba5e5c4c13c4276927c17a7a2fcd
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-06-08 07:05:08 +0200

    feat: ajout des composant au lieu des imports

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,46 +1,52 @@
+import ComptePageVue from 'src/pages/ComptePage.vue';
+import ConnexionPageVue from 'src/pages/ConnexionPage.vue';
+import ContactPageVue from 'src/pages/ContactPage.vue';
+import HomePageVue from 'src/pages/HomePage.vue';
+import InscriptionPageVue from 'src/pages/InscriptionPage.vue';
+import QuiSommesNousVue from 'src/pages/QuiSommesNous.vue';
+import RecherchePageVue from 'src/pages/RecherchePage.vue';
 import { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name:'Home',
-    component: () => import('pages/HomePage.vue'),
+    component: HomePageVue,
   },
   {
     path: '/qui-sommes-nous',
     name: 'QuiSommeNous',
-    component: () => import('pages/QuiSommesNous.vue'),
+    component: QuiSommesNousVue,
   },
   {
     path: '/contact',
     name: 'Contact',
-    component: () => import('pages/ContactPage.vue')
+    component: ContactPageVue
   },
   {
     path: '/mon-compte',
     name: 'Compte',
-    component: () => import('pages/ComptePage.vue')
+    component: ComptePageVue
   },
   {
     path: '/connexion',
     name: 'Login',
-    component: () => import('pages/ConnexionPage.vue')
+    component: ConnexionPageVue
   },
   {
     path: '/inscription',
-    component: () => import('pages/InscriptionPage.vue')
+    name: 'Inscription',
+    component: InscriptionPageVue
   },
   {
     path: '/recherche',
-    component: () => import('pages/RecherchePage.vue')
+    component: RecherchePageVue
   },
   {
     path: '/test',
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
   },
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue'),
