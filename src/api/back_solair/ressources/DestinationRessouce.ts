164cbb43f57d274fc04bcbc361425a45ec7b2850
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-06-08 06:55:34 +0200

    feat: ajout de Destination Ressource

---
+++ src/api/back_solair/ressources/DestinationRessouce.ts
@@ -0,0 +1,13 @@
+import { httpClient } from '../httpClient';
+import { Destination } from '../models/Destination';
+
+const ressource = 'api/destinations'
+
+class DestinationRessource {
+    public async getDestinationsAsync(): Promise<Destination[]> {
+        const response = await httpClient.get(`${ressource}`)
+        return response.data as Destination[]
+    }
+}
+
+export {DestinationRessource}
