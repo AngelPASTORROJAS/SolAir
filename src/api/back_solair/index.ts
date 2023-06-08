e017fec5c7ca65359dce5c6accddb12e7d1742e0
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-06-08 06:57:00 +0200

    feat: ajout de DestinationRessource à l'api  solairApi

--- src/api/back_solair/index.ts
+++ src/api/back_solair/index.ts
@@ -1,10 +1,13 @@
+import { DestinationRessource } from './ressources/DestinationRessouce';
 import { UserRessource } from './ressources/UserRessource';
 
 interface SolairApiType {
   user: UserRessource
+  destination: DestinationRessource
 }
 
 const solairAPI: SolairApiType = {
-  user: new UserRessource()
+  user: new UserRessource(),
+  destination: new DestinationRessource()
 };
 export { solairAPI };
