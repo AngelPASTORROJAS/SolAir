5e44abb3870b059a71fe38b51709476f07964825
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-07-04 00:11:18 +0200

    feat: ajout le logout dans la navBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,13 @@
 <template>
   <nav class="q-py-md q-px-xl navigation text-custom-p">
     <ul class="row items-center justify-between navigateur-list">
-      <li class=" q-pt-sm q-pl-md">
+      <li class="q-pt-sm q-pl-md">
         <a class="text-nav" href="#">
           <img src="./../assets/img/logo.svg" alt="logo du site" />
         </a>
       </li>
       <li>
-        <a class="gt-sm text-white text-nav" href="#/recherche">Gestionnaire d'experience</a>
+        <a class="gt-sm text-white text-nav" href="#/recherche">Gestionnaire d'expérience</a>
       </li>
       <li>
         <a class="gt-sm text-white text-nav" href="#/qui-sommes-nous">Qui sommes-nous ?</a>
@@ -16,16 +16,53 @@
         <a class="gt-sm text-white text-nav" href="#/contact">Contact</a>
       </li>
       <li>
-        <q-btn class=" button-custom" to="/connexion" size="30px" flat round icon="perm_identity" />
+        <q-btn class="button-custom" @click="navigateTo" size="30px" flat round :icon="isConnect ? 'logout' : 'perm_identity'" />
       </li>
     </ul>
   </nav>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted, ref } from 'vue';
+import { useAuthStore } from 'src/stores/auth-store';
+import { useRouter } from 'vue-router';
+
 export default defineComponent({
   name: 'NavBarComponent',
+  setup() {
+    const route = useRouter();
+
+    const authStore = useAuthStore();
+    const isConnect = ref(false);
+
+    const logOut = () => {
+      isConnect.value = false;
+      authStore.logout();
+    };
+
+    const navigateTo = () => {
+      if (isConnect.value) {
+        logOut();
+        route.push({name: 'Home'});
+      } else {
+        route.push({name: 'Login'});
+      }
+    };
+
+    onMounted(async () => {
+      try {
+        isConnect.value = await authStore.IsConnect();
+      } catch (error) {
+        console.error(error);
+        isConnect.value = false;
+      }
+    });
+
+    return {
+      isConnect,
+      navigateTo,
+    };
+  },
   components: {},
 });
 </script>
@@ -49,6 +86,7 @@ export default defineComponent({
   line-height: 22px;
   text-decoration: none;
 }
+
 .text-nav:hover {
   text-decoration: underline;
   text-decoration-color: $titre;
