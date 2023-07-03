c3b8164e4c3226d4ed9606c7d84026b848647014
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-07-03 15:26:27 +0200

    feat: Ajout de variable d'environnement API_ROUTE

--- src/api/back_solair/httpClient.ts
+++ src/api/back_solair/httpClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const httpClient = axios.create({
-  baseURL: 'http://localhost:3000/'
+  baseURL: process.env.API_ROUTE
 })
 
 const httpStatusCode = axios.HttpStatusCode
