9c9de93160edb85e6cd2002584875bd0c57099a3
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-07-04 08:51:29 +0200

    fix: suppression de appel api de test pour récuperer tous les utilisateur

--- src/api/back_solair/ressources/UtilisateurRessource.ts
+++ src/api/back_solair/ressources/UtilisateurRessource.ts
@@ -5,11 +5,6 @@ import { LocalStorage } from 'quasar';
 const ressource = 'api/utilisateurs';
 
 class UtilisateurRessource {
-  public async getUsersAsync(): Promise<Utilisateur[]> {
-    const response = await httpClient.get(`${ressource}`);
-    return response.data as Utilisateur[];
-  }
-
   public async createUtilisateurAsync(utilisateur: Utilisateur): Promise<boolean> {
     const response = await httpClient.post(`${ressource}`, utilisateur);
     const token = response.data.token;
