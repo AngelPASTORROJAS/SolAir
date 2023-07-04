9b59c445defaf0d814307b7a51bded009a0c5b5d
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-07-04 09:18:40 +0200

    style: suppression d'espace blanc

--- src/components/HeaderCustom.vue
+++ src/components/HeaderCustom.vue
@@ -1,42 +1,41 @@
 <template>
-    <div class="accueil-div">
-      <NavBar />
-      <h1 class="full-width text-white text-center text-bold accueil">
-        {{ label }}
-      </h1>
-    </div>
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent } from 'vue';
-  import NavBar from './NavBar.vue';
-  export default defineComponent({
-    name: 'HeaderCustom',
-    components: {
-      NavBar,
+  <div class="accueil-div">
+    <NavBar />
+    <h1 class="full-width text-white text-center text-bold accueil">
+      {{ label }}
+    </h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import NavBar from './NavBar.vue';
+export default defineComponent({
+  name: 'HeaderCustom',
+  components: {
+    NavBar,
+  },
+  props: {
+    label: {
+      type: String,
+      required: true,
     },
-    props: {
-      label: {
-        type: String,
-        required: true,
-      },
-    },
-  });
-  </script>
-  
-  <style lang="scss" scoped>
-  .accueil-div {
-    background: url('./../assets/img/header.jpg') center / cover no-repeat,
-      url('./../assets/img/fumee.png') center / cover no-repeat;
-    background-blend-mode: screen;
-  }
-  
-  .accueil {
-    padding-bottom: 25vh;
-    font-family: 'Montserrat';
-    text-transform: capitalize;
-    margin: 10% 20%;
-    margin-bottom: 20px;
-  }
-  </style>
-  
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.accueil-div {
+  background: url('./../assets/img/header.jpg') center / cover no-repeat,
+    url('./../assets/img/fumee.png') center / cover no-repeat;
+  background-blend-mode: screen;
+}
+
+.accueil {
+  padding-bottom: 25vh;
+  font-family: 'Montserrat';
+  text-transform: capitalize;
+  margin: 10% 20%;
+  margin-bottom: 20px;
+}
+</style>
